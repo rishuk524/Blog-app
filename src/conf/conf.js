28d0927147a315645dcfd223c65e2a67dba954b1
rishuk524 <rishuk524@gmail.com>
2024-02-29 14:50:31 +0530

    p stopped

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,7 @@ const conf = {
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-     appwriteApiKey: String(import.meta.env.VITE_APPWRITE_API_kEY),
+    //  appwriteApiKey: String(import.meta.env.VITE_APPWRITE_API_kEY),
 } 
 
 export default conf
